3b8919bad7a33464bb2fc5756ba2d878202b33d7
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2024-08-18 09:05:23 -0700

    refactor

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -104,3 +104,10 @@ const timingMiddleware = t.middleware(async ({ next, path }) => {
  * are logged in.
  */
 export const publicProcedure = t.procedure.use(timingMiddleware);
+
+export const createInnerTRPCContext = () => {
+  return {
+    headers: Headers.prototype,
+    db,
+  };
+};
