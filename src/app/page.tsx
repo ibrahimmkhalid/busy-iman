d55511d4772beb7b27a8a32f52ee61951c733e0c
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2024-09-14 23:44:01 -0700

    add timer and timeline pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,5 @@
-import React from "react";
-import Canvas from "~/components/canvas";
-import CurrentPrayer from "~/components/currentPrayer";
-import PrayerCounter from "~/components/prayerCounter";
-import { api } from "~/trpc/server";
+import { redirect } from "next/navigation";
 
-const now = new Date().toISOString();
 export default async function App() {
-  const data = await api.prayer.getPrayerTimings({
-    date: now,
-    long: -122.0085,
-    lat: 37.5339,
-    elevation: 0,
-    tz: -7,
-    asr2: false,
-    fajrAngle: 15,
-    ishaAngle: 15,
-  });
-
-  return (
-    <Canvas>
-      <CurrentPrayer prayerTimings={data} />
-      <PrayerCounter prayerTimings={data} />
-    </Canvas>
-  );
+  redirect("/timer");
 }
